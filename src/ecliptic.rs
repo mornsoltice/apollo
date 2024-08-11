4ff1af652deebc4726debc8000c3b94f3886c72a
mornsoltice <kafianjingkucingbebek@gmail.com>
2024-08-12 05:35:12 +0700

    i love venti

--- src/ecliptic.rs
+++ src/ecliptic.rs
@@ -52,7 +52,7 @@ pub fn mean_obliquity_laskar(JD: f64) -> f64 {
         angle::deg_dmas(0,  0,  1999.25),
        -angle::deg_dmas(0,  0,  51.38),
        -angle::deg_dmas(0,  0,  249.67),
-       -angle::deg_dms(0,  0,  39.05),
+       -angle::deg_dmas(0,  0,  39.05),
         angle::deg_dmas(0,  0,  7.12),
         angle::deg_dmas(0,  0,  27.87),
         angle::deg_dmas(0,  0,  5.79),
@@ -60,7 +60,7 @@ pub fn mean_obliquity_laskar(JD: f64) -> f64 {
     ).to_radians()
 }
 
-/**
+/*
 Computes the mean obliquity of the ecliptic using
 the IAU formula
 
@@ -75,11 +75,65 @@ The error in `mn_oblq` reaches 1 arcsecond over a period of
 # Arguments
 
 * `JD`: Julian (Ephemeris) day
-**/
+*/
 
 pub fn mean_obliquity_IAU(JD: f64) -> f64 {
-    let u = time::julian_century(JD);
+    let u = time::julian_century(JD) / 100.00;
+    Horner_eval!(
+        u,
+        angle::deg_dmas(23, 26, 21.448),
+       -angle::deg_dmas(0,  0,  46.815),
+       -angle::deg_dmas(0,  0,  0.00059),
+        angle::deg_dmas(0,  0,  0.001813)
+    ).to_radians()
 }
 
+/*
+Computes the longitudes of the two ecliptic points on
+a horizon on Earth
 
+# Returns
+
+`(long_point_1, long_point_2)`
+
+* `long_point_1`: Longitude of ecliptic point 1 *| in radians*
+* `long_point_2`: Longitude of ecliptic point 2 *| in radians*
+
+# Arguments
+
+* `oblq_eclip`  : Obliquity of the ecliptic *| in radians*
+* `observer_lat`: The observer's geographical latitude *| in radians*
+* `loc_sidreal` : Local sidereal time *| in radians*
+*/
+pub fn eclip_points_on_hz(oblq_eclip: f64, observer_lat: f64, loc_sidreal: f64) -> (f64, f64) {
+    let p = (-loc_sidreal.cos()).atan2 (
+        oblq_eclip.sin() * observer_lat.tan()
+      + oblq_eclip.cos() * loc_sidreal.sin()
+    );
+
+    (p, p + PI)
+}
+
+/*
+Computes the angle between the ecliptic and a horizon
+on Earth
+
+# Returns
+
+* `angle`: Angle between the ecliptic and the horizon *| in radians*
+
+# Arguments
+
+* `oblq_eclip`  : Obliquity of the ecliptic *| in radians*
+* `observer_lat`: The observer's geographical latitude *| in radians*
+* `loc_sidreal` : Local sidereal time *| in radians*
+*/
+pub fn angl_betwn_eclip_and_hz(oblq_eclip: f64, observer_lat: f64,
+                              loc_sidreal: f64) -> f64
+{
+    (
+        oblq_eclip.cos() * observer_lat.sin()
+      - oblq_eclip.sin() * observer_lat.cos() * loc_sidreal.sin()
+    ).acos()
+}
 
