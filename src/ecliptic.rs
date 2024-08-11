77676548b92920f4844cae8f439e065b195c3e84
mornsoltice <kafianjingkucingbebek@gmail.com>
2024-08-11 14:08:38 +0700

    i love venti

--- src/ecliptic.rs
+++ src/ecliptic.rs
@@ -42,4 +42,44 @@ for 10000 years before and after 2000 AD.
 * `JD`: Julian (Ephemeris) day
 */
 
+pub fn mean_obliquity_laskar(JD: f64) -> f64 {
+    let u = time::julian_century(JD);
+    Horner_eval!(
+        u,
+        angle::deg_dmas(23, 26, 21.448),
+       -angle::deg_dmas(0,  0,  4680.93),
+       -angle::deg_dmas(0,  0,  1.55),
+        angle::deg_dmas(0,  0,  1999.25),
+       -angle::deg_dmas(0,  0,  51.38),
+       -angle::deg_dmas(0,  0,  249.67),
+       -angle::deg_dms(0,  0,  39.05),
+        angle::deg_dmas(0,  0,  7.12),
+        angle::deg_dmas(0,  0,  27.87),
+        angle::deg_dmas(0,  0,  5.79),
+        angle::deg_dmas(0,  0,  2.45)
+    ).to_radians()
+}
+
+/**
+Computes the mean obliquity of the ecliptic using
+the IAU formula
+
+# Returns
+
+* `mn_oblq`: Mean obliquity of the ecliptic *| in radians*
+
+The error in `mn_oblq` reaches 1 arcsecond over a period of
+2000 years from 2000 AD, and about 10 arcseconds over a period of
+4000 years from 2000 AD.
+
+# Arguments
+
+* `JD`: Julian (Ephemeris) day
+**/
+
+pub fn mean_obliquity_IAU(JD: f64) -> f64 {
+    let u = time::julian_century(JD);
+}
+
+
 
