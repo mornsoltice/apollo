f6f1ef5bcf2f6bd52d1227aec8210c7fd5b3d2ef
mornsoltice <kafianjingkucingbebek@gmail.com>
2024-08-12 11:11:55 +0700

    i love venti

--- src/atmospheric.rs
+++ src/atmospheric.rs
@@ -45,3 +45,117 @@ pub fn refrac_apparent_altitude_15(apprnt_alt: f64) -> f64 {
 
     angle::deg_dmas(0, 0, 58.294).to_radians() * (PI - apprnt_alt).tan() - x * x * x
 }
+
+/*
+Computes the refraction term for apparent altitudes greater than 15
+degrees
+
+# Returns
+
+* `refrac_term`: The refraction term *| in radians*, that needs to be
+                 added to the true altitude to get the apparent
+                 altitude
+
+# Arguments
+
+* `true_alt`: True altitude *| in radians*
+*/
+
+pub fn refrac_true_altitude_15(true_alt: f64) -> f64 {
+    let x = angle::deg_dmas(0, 0, 0.0824).to_radians() * (PI - true_alt).tan();
+
+    angle::deg_dmas(0, 0, 58.276).to_radians() * (PI - true_alt).tan() - x * x * x
+}
+
+/*
+Computes the refraction term for true altitude
+
+# Returns
+
+* `refrac_term`: The refraction term *| in radians*, that needs to be
+                 subtracted from the apparent altitude to get the
+                 rue altitude
+
+The accuracy of `refrac_term` is upto 0.07 arcminutes.
+
+# Arguments
+
+* `apprnt_alt`: Apparent altitude *| in radians*
+*/
+
+pub fn refrac_apparent_altitude(apprnt_alt: f64) -> f64 {
+    if apprnt_alt == PI {
+        0.0
+    } else {
+        let apprnt_alt_deg = apprnt_alt.to_degrees();
+        let a = apprnt_alt_deg + 7.31 / (apprnt_alt_deg + 4.4);
+        let R = 1.0 / a.to_radians().tan();
+
+        (R / 60.0).to_radians()
+    }
+}
+
+/*
+Computes the refraction term for apparent altitude
+
+This function is consistent with `refrac_frm_apprnt_alt()` to
+within 4 arcseconds.
+
+# Returns
+
+* `refrac_term`: The refraction term *| in radians*, that needs to be
+                 added to the true altitude to get the apparent
+                 altitude
+
+# Arguments
+
+* `true_alt`: True altitude *| in radians*
+*/
+
+pub fn refrac_true_alt(true_alt: f64) -> f64 {
+    if true_alt == PI {
+        0.0
+    } else {
+        let true_alt_deg = true_alt.to_degrees();
+        let a = true_alt_deg + 10.3 / (true_alt_deg + 5.11);
+        let R = 1.02 / a.to_radians().tan();
+
+        (R / 60.0).to_radians()
+    }
+}
+
+/*
+Computes the refraction term modifier for local pressure
+
+# Returns
+
+* `refrac_term_modifier`: The value that needs to be multiplied by
+                          the refraction term to account for local
+                          pressure
+
+# Arguments
+
+* `pressure`: Local pressure *| in millibars*
+*/
+
+pub fn refrac_pressure(pressure: f64) -> f64 {
+    pressure / 1010.0
+}
+
+/*
+Computes the refraction term modifier for local temperature
+
+# Returns
+
+* `refrac_term_modifier`: The value that needs to be multiplied by
+                          the refraction term to account for local
+                          temperature
+
+# Arguments
+
+* `temp`: Local temperature *| in kelvins*
+*/
+#[inline(always)]
+pub fn refrac_temp(temp: f64) -> f64 {
+    283.0 / temp
+}
